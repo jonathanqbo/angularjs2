ecf1988ea95f1c0e333b339575a6ed2b4e4f1799
qibo <jonathan.q.bo@gmail.com>
2016-10-26 10:16:36 -0400

    refactor 2:
    -- ngFor: show list of heroes
    -- ngIf: show only if li is selected
    -- style class when selected

--- app/app.component.ts
+++ app/app.component.ts
@@ -6,25 +6,50 @@ export class Hero {
 	name: string;
 }
 
+const HEROES: Hero[] = [
+	{id: 11, name: 'Mr. Nice'},
+	{id: 12, name: 'Narco'},
+	{id: 13, name: 'Bombasto'},
+	{id: 14, name: 'Celerita'},
+	{id: 15, name: 'Mageta'},
+	{id: 16, name: 'RubberMan'},
+	{id: 17, name: 'Dynama'},
+	{id: 18, name: 'Dr QI'},
+	{id: 19, name: 'Magma'},
+	{id: 20, name: 'Tornado'}
+]
+
 @Component({
   selector: 'my-app',
   template: `<h1>{{title}}</h1>
-	  		 <h2>{{hero.name}} details</h2>
-			 <div><label>id: </label>{{hero.id}}</div>
-			 <div>
-			 	<label>name: </label>
-			 	<input [(ngModel)]="hero.name" placeholder="name">
-			 </div>
+	  		 <h2>My Heroes</h2>
+	  		 <ul class="heroes">
+	  		 	<li *ngFor = "let hero of heroes" (click)="onSelect(hero)" [class.selected]="hero === selectedHero">
+	  		 		<span class="badge">{{hero.id}}</span> {{hero.name}}
+	  		 	</li>
+	  		 </ul>
+	  		 <div *ngIf="selectedHero">
+		  		 <h2>{{selectedHero.name}} details!</h2>
+		  		 <div><label>id: </label>{{selectedHero.id}}</div>
+		  		 <div>
+		  		 	<label>name: </label>
+		  		 	<input [(ngModel)]="selectedHero.name" placeholder="Hero Name"/>
+		  		 </div>
+		  	</div>
 			`
 })
 
 export class AppComponent {
 	
-	hero: Hero = {
-		id: 1,
-		name: 'Windstorm'
-	};
+	heroes = HEROES;
 
 	title = 'Tour of Heroes';
+	
+	selectedHero: Hero;
+	
+	onSelect(hero: Hero): void {
+		this.selectedHero = hero;
+	}
+	
 	
 }
