6f7c6414e3995910bb75bdd7ffa1f8316da1dc2d
qibo <jonathan.q.bo@gmail.com>
2016-10-26 10:36:31 -0400

    seperate components to make it reusable
    -- seperated entity object: hero
    -- seperated component: hero-detail, with an Input attribute which value
    is from outside
    -- declare new component in app.module.ts

--- app/app.component.ts
+++ app/app.component.ts
@@ -1,10 +1,6 @@
 import { Component } from '@angular/core';
 
-// NOTE: need be defined before @Component to make it work
-export class Hero {
-	id: number;
-	name: string;
-}
+import { Hero } from './hero';
 
 const HEROES: Hero[] = [
 	{id: 11, name: 'Mr. Nice'},
@@ -28,14 +24,7 @@ const HEROES: Hero[] = [
 	  		 		<span class="badge">{{hero.id}}</span> {{hero.name}}
 	  		 	</li>
 	  		 </ul>
-	  		 <div *ngIf="selectedHero">
-		  		 <h2>{{selectedHero.name}} details!</h2>
-		  		 <div><label>id: </label>{{selectedHero.id}}</div>
-		  		 <div>
-		  		 	<label>name: </label>
-		  		 	<input [(ngModel)]="selectedHero.name" placeholder="Hero Name"/>
-		  		 </div>
-		  	</div>
+	  		 <my-hero-detail [hero]=selectedHero></my-hero-detail>
 			`
 })
 
